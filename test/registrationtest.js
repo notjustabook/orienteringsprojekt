521065af13042b715b37766dfde54d602dfb2d93
MekaNekodemo <martinTeitBuchwald@gmail.com>
2019-11-20 21:03:24 +0100

    Before Regi methods in Controller work. test works = false.

--- test/registrationtest.js
+++ test/registrationtest.js
@@ -5,21 +5,30 @@ let controller = require('../controllers/controller');
 // this test requires a user and a user with a ride.
 describe('Registration', async () => {
    //Create user and save to database
-   let name = 'John Nielsen';
-   let userName = 'JN1944';
-   let password = 'John1234';
-   await controller.createUser(name, userName, password);
+   let name01 = 'John Nielsen';
+   let username01 = 'JN1944';
+   let password01 = 'John1234';
+   await controller.createUser(name01, username01, password01);
 
    //create user and save to DB, then Create a ride associated with this user.
    let name = 'Ride Holdenson';
-   let userName = 'xRHx';
+   let username = 'xRHx';
    let password = 'xxxxxx';
-   await controller.createUser(name, userName, password);
+   await controller.createUser(name, username, password);
 
    //^creating ride
    let pPoint = 'pick up here!';
    let noOfSeats = 5;
-   await controller.createRide( )
+   await controller.createRide( );
+
+   it('Should save to MongoDB',  async function() {
+         
+   });
+
+
+
+
+
 
 });
 
